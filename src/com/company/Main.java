9d4fd91bdbc45e62e45d795863fce07f9a0b9c38
eg913 <eg913@pvsd.org>
2018-09-17 11:12:55 -0400

    still needs work

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner word1Reader = new Scanner(System.in);
+        System.out.println("Enter your first word here: ");
+        String word1 = word1Reader.nextLine();
+        Scanner word2Reader = new Scanner(System.in);
+        System.out.println("Enter your second word here: ");
+        String word2 = word2Reader.nextLine();
+        if (word1.length() == word2.length());
+
+
+
+
+
     }
 }
