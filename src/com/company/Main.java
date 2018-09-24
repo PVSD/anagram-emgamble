c3cff9c1f2be55b25064f4a4915b4dbba7fdf28f
eg913 <eg913@pvsd.org>
2018-09-23 20:36:36 -0400

    Should work even with double letters

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,10 +8,57 @@ public class Main {
         Scanner word1Reader = new Scanner(System.in);
         System.out.println("Enter your first word here: ");
         String word1 = word1Reader.nextLine();
+
         Scanner word2Reader = new Scanner(System.in);
         System.out.println("Enter your second word here: ");
         String word2 = word2Reader.nextLine();
-        if (word1.length() == word2.length());
+
+        int count = 0;
+        int doub = 0;
+        int doubfix = 0;
+
+        if (word1.length() == word2.length()) {
+            char[] worda = word1.toCharArray();
+            char[] wordb = word2.toCharArray();
+
+            for (int j = 0; j < word1.length(); j++) {
+                for (int k = 0; k < word1.length(); k++)
+                    if (worda[j] == wordb[k])
+                        count++;
+            }
+
+            for (int m = 0; m < word1.length(); m++) {
+                for (int n = 0; n < word1.length(); n++)
+                    if (worda[n] == worda[m])
+                        doub++;
+
+            }
+
+
+            //System.out.println(doub);
+
+            doubfix = doub - word1.length();
+            count = count - doubfix;
+           // System.out.println(doubfix);
+            //System.out.println(count);
+
+            if (count >= word1.length())
+                System.out.println(word1 + " and " + word2 + " are anagrams.");
+            else
+                System.out.println(word1 + " and " + word2 + " are not anagrams.");
+
+
+
+        }
+        else
+            System.out.println(word1 + " and " + word2 + " are not anagrams.");
+
+
+
+
+
+
+
 
 
 
